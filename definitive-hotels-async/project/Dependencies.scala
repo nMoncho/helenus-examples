453aa5b0ff5c6aa903c065fe270e722ac55bfd77
Gustavo De Micheli <gustavo.demicheli@gmail.com>
2024-08-27 13:47:13 +0200

    Update examples to v1.3.1

--- definitive-hotels-async/project/Dependencies.scala
+++ definitive-hotels-async/project/Dependencies.scala
@@ -1,8 +1,8 @@
 import sbt._
 
 object Dependencies {
-  lazy val helenus       = "net.nmoncho"      %% "helenus-core"     % "1.0.0"
-  lazy val dseJavaDriver = "com.datastax.oss"  % "java-driver-core" % "4.15.0"
+  lazy val helenus       = "net.nmoncho"      %% "helenus-core"     % "1.3.1"
+  lazy val dseJavaDriver = "com.datastax.oss"  % "java-driver-core" % "4.17.0"
   lazy val cassandraUnit = "org.cassandraunit" % "cassandra-unit"   % "4.3.1.0"
   lazy val scalaTest     = "org.scalatest"    %% "scalatest"        % "3.2.15"
   lazy val jna           = "net.java.dev.jna"  % "jna"              % "5.12.1" // Fixes M1 JNA issue
